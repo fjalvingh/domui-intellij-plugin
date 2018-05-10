68dba4fb4e2fd62810004f8c71beb8f0cd2d1023
Frits Jalvingh <jal@etc.to>
2018-05-10 21:04:41 +0200

    Add DomUI icon to IntelliJ refactor icon for now.

---
+++ src/icons/DomuiPluginIcons.java
@@ -0,0 +1,13 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * @author <a href="mailto:jal@etc.to">Frits Jalvingh</a>
+ * Created on 10-5-18.
+ */
+public interface DomuiPluginIcons {
+	Icon FIX_PROPERTIES = IconLoader.getIcon("/icons/fixProperties.png");
+}
